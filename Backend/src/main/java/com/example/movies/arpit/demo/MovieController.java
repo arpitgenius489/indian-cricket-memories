664018321cba711b431b6a16182c479a134a2702
Arpit Yadav <www.arpitgenius489@gmail.com>
2025-01-19 00:10:37 +0530

    reverted cors info and updated with docker

--- Backend/src/main/java/com/example/movies/arpit/demo/MovieController.java
+++ Backend/src/main/java/com/example/movies/arpit/demo/MovieController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController 
 @RequestMapping("/api/v1/movies")
-@CrossOrigin(origins = "https://indian-cricket-memories.onrender.com")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
     @Autowired
     private MovieService movieService;
