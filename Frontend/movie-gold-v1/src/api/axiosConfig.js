42e561f035a58961305d60832578a86c37b51b8b
Arpit Yadav <www.arpitgenius489@gmail.com>
2025-01-26 13:13:08 +0530

    updated to use .env things and

--- Frontend/movie-gold-v1/src/api/axiosConfig.js
+++ Frontend/movie-gold-v1/src/api/axiosConfig.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
-console.log('Current Environment:', process.env.NODE_ENV);
-console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+console.log('Current API URL:', process.env.REACT_APP_API_BASE_URL);
 
-const axiosInstance = axios.create({
-    baseURL: API_BASE_URL || 'http://localhost:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+export default axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    headers: {"ngrok-skip-browser-warning": "true"}
 });
-
-export default axiosInstance;
