f76fc36dc9a12b74b4fa579b3d7dfd3468e670ef
Arpit Yadav <www.arpitgenius489@gmail.com>
2025-01-26 10:37:57 +0530

    add .gitignore to update .env and also change the axiosConfig.js

--- Frontend/movie-gold-v1/src/api/axiosConfig.js
+++ Frontend/movie-gold-v1/src/api/axiosConfig.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+console.log('Current Environment:', process.env.NODE_ENV);
+console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export default axios.create({
-    baseURL: API_BASE_URL,
-    headers: {"ngrok-skip-browser-warning": "true"}
+const axiosInstance = axios.create({
+    baseURL: API_BASE_URL || 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
+
+export default axiosInstance;
