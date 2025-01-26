3726d4528f6aae54ee6274e2243a5cf7503abf06
Arpit Yadav <www.arpitgenius489@gmail.com>
2025-01-26 13:25:12 +0530

    just to remove the untracked file message doing this

--- Frontend/movie-gold-v1/src/api/axiosConfig.js
+++ Frontend/movie-gold-v1/src/api/axiosConfig.js
@@ -4,5 +4,5 @@ console.log('Current API URL:', process.env.REACT_APP_API_BASE_URL);
 
 export default axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
-    headers: {"ngrok-skip-browser-warning": "true"}
+    headers: {"ngrok-skip-browser-warning": "false"}
 });
