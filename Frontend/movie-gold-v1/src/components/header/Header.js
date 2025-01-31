f3f16903963de5bd1fca2c75e9c4dd872279bac1
Arpit Yadav <www.arpitgenius489@gmail.com>
2025-01-31 21:29:06 +0530

    updated Dockerfile

--- Frontend/movie-gold-v1/src/components/header/Header.js
+++ Frontend/movie-gold-v1/src/components/header/Header.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import {NavLink} from "react-router-dom";
+import { ReactComponent as WebsiteLogo } from "../../assets/white-cricket-logo.svg"
 
 const Header = () => {
  
@@ -12,15 +13,16 @@ return (
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
             <Navbar.Brand href="/" style={{"color":'gold'}}>
-                <FontAwesomeIcon icon ={faVideoSlash}/>Gold
+                <WebsiteLogo style={{ width: '72px', height: '72px'}}/> Indian Cricket Memories
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
+            
             <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{maxHeight: '100px'}}
-                        navbarScroll
-                    >
+                <Nav
+                    className="mx-auto my-2 my-lg-0"
+                    style={{maxHeight: '100px'}}
+                    navbarScroll
+                >
                     <NavLink className ="nav-link" to="/">Home</NavLink>
                     <NavLink className ="nav-link" to="/watchList">Watch List</NavLink>      
                 </Nav>
